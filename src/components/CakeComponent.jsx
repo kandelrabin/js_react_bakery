0d5b155656e685175d175a2470c0057b181b34f5
Rabin Kandel <kandelrabin@live.com>
2024-06-26 18:17:41 +0100

    add sale cake button to each cakeComponent

--- src/components/CakeComponent.jsx
+++ src/components/CakeComponent.jsx
@@ -1,11 +1,33 @@
-const CakeComponent = ({name, ingredients, price, rating}) => {
+const CakeComponent = ({name, ingredients, price, rating, quantity, countClick, totalValue, sellClick}) => {
     const cakeId = name.toLowerCase().replace(" ", "-");
+
+    const handleAddCountClick = () => {
+        countClick(quantity + 1);
+    };
+
+    const handleRemoveCountClick = () => {
+        if (quantity <= 0) return;
+        countClick(quantity - 1);
+    }
+
+    const handleSaleClick = () => {
+        sellClick(totalValue + quantity * price);
+        console.log("sell price :"+totalValue + quantity * price);
+    }
+
     return (
         <div id={cakeId}>
             <h3>{name}</h3>
             <p>Ingredients: {ingredients.join(", ")}</p>
             <p>Price: £{price.toFixed(2)}</p>
             <p>Rating: {rating.toFixed(1)}</p>
+            <div id="quantity">
+                <p>Quantity:{"  "+quantity}</p>
+                <button onClick={handleAddCountClick}>+</button>
+                <button onClick={handleRemoveCountClick}>-</button>
+            </div>
+
+            <button onClick={handleSaleClick}>sell cake</button>
         </div>
     );
 }
--- src/containers/BakeryContainer.jsx
+++ src/containers/BakeryContainer.jsx
@@ -2,29 +2,36 @@ import CakeComponent from "../components/CakeComponent";
 import { useState } from "react";
 
 const BakeryContainer = () => {
-    const [victoriaSpongePrice] = useState(5);
-    const [victoriaSpongeRating] = useState(5);
-    const [teaLoafPrice] = useState(2);
-    const [teaLoafRating] = useState(3);
-    const [carrotCakePrice] = useState(8);
-    const [carrotCakeRating] = useState(1);
+    const victoriaSpongePrice = 5;
+    const victoriaSpongeRating = 5;
+    const teaLoafPrice = 2;
+    const teaLoafRating = 3;
+    const carrotCakePrice = 8;
+    const carrotCakeRating = 1;
     const averageRating = ((victoriaSpongeRating + teaLoafRating + carrotCakeRating) / 3);
+
+    const [victoriaSpongeCounter, setvictoriaSpongeCounter] = useState(0);
+    const [teaLoafCounter, setTeaLoafCounter] = useState(0);
+    const [carrotCakeCounter, setCarrotCakeCounter] = useState(0);
+    const [totalValue, setTotalValue] = useState(0);
 
     return (
         <>
             <h2>Cakes:</h2>
             <hr />
             <section className="cake">
-                <CakeComponent name = {"Victoria Sponge"} ingredients = {["eggs", "butter", "sugar", "self-raising flour", "baking powder", "milk"]} price = {victoriaSpongePrice} rating = {victoriaSpongeRating} />
+                <CakeComponent name = {"Victoria Sponge"} ingredients = {["eggs", "butter", "sugar", "self-raising flour", "baking powder", "milk"]} price = {victoriaSpongePrice} rating = {victoriaSpongeRating} quantity = {victoriaSpongeCounter} countClick = {setvictoriaSpongeCounter} totalValue = {totalValue} sellClick = {setTotalValue}/>
 
-                <CakeComponent name = {"Tea Loaf"} ingredients = {["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"]} price = {teaLoafPrice} rating = {teaLoafRating} />
+                <CakeComponent name = {"Tea Loaf"} ingredients = {["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"]} price = {teaLoafPrice} rating = {teaLoafRating} quantity = {teaLoafCounter} countClick={setTeaLoafCounter} totalValue = {totalValue} sellClick = {setTotalValue}/>
 
-                <CakeComponent name = {"Carrot Cake"} ingredients = {["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"]} price = {carrotCakePrice} rating = {carrotCakeRating} />
+                <CakeComponent name = {"Carrot Cake"} ingredients = {["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"]} price = {carrotCakePrice} rating = {carrotCakeRating} quantity={carrotCakeCounter} countClick={setCarrotCakeCounter} totalValue = {totalValue} sellClick = {setTotalValue}/>
             </section>
             <hr />
             
 
             <p>Average rating: {averageRating.toFixed(1)}</p>
+            <hr />
+            <p id="total">Total value: £{totalValue.toFixed(2)}</p>
         </>
     );
 }
