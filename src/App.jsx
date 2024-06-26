585b5f442606551ec8fd9f9cd249f289c80d538b
Rabin Kandel <kandelrabin@live.com>
2024-06-26 17:25:55 +0100

    add css to the app - extension 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <h1>Bakery</h1>
-    <BakeryContainer />
+    <BakeryContainer id="bakery-container"/>
     </>
   )
 }
--- src/components/CakeComponent.jsx
+++ src/components/CakeComponent.jsx
@@ -1,12 +1,13 @@
 const CakeComponent = ({name, ingredients, price, rating}) => {
+    const cakeId = name.toLowerCase().replace(" ", "-");
     return (
-        <>
+        <div id={cakeId} className="cake">
             <h3>{name}</h3>
             <p>Ingredients: {ingredients.join(", ")}</p>
             <p>Price: £{price.toFixed(2)}</p>
             <p>Rating: {rating.toFixed(1)}</p>
             <hr />
-        </>
+        </div>
     );
 }
 
